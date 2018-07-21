0d9118f1376e97da99f027b6f38b71b7a7be260e
zhujunrong <15627983535@163.com>
2018-07-21 16:16:13 +0800

    web start

--- src/main/java/com/zhu/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/zhu/springbootdemo/SpringbootdemoApplication.java
@@ -2,11 +2,21 @@ package com.zhu.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootdemoApplication.class, args);
 	}
+
+	@RequestMapping("/helloWorld")
+	public String helloWorld(){
+		return "hello world!";
+	}
 }
+
